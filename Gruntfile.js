b5db7e82133aaa4188045fc7cf4a2b989c225d42
Christopher Torgalson <manager@bedlamhotel.com>
2014-09-21 20:00:41 +0200

    Add copy rule to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,20 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    copy: {
+      js: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/foundation/js/vendor/',
+            src: '**',
+            dest: 'js/',
+            flatten: true,
+            filter: 'isFile'
+          }
+        ]
+      }
+    }
+  })
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.registerTask("default", ["copy"]);
 }
