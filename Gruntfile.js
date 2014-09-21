09f2b4f2fbac924fd2667adfeb2e80ae932e79e8
Christopher Torgalson <manager@bedlamhotel.com>
2014-09-21 22:35:10 +0200

    Set up compass, exec and concurrent in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,38 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    compass: {
+      options: {
+        cssDir: 'css',
+        environment: 'development',
+        fontsDir: 'fonts',
+        httpPath: '/',
+        imagesDir: 'img',
+        importPath: 'bower_components/foundation/scss',
+        javascriptsDir: 'js',
+        noLineComments: true,
+        outputStyle: 'nested',
+        relativeAssets: true,
+        sassDir: '_scss',
+        watch: false
+      },
+      dev: {
+        options: {
+          outputStyle: 'nested',
+          watch: true
+        }
+      }
+    },
+    concurrent: {
+      options: {
+        limit: 5,
+        logConcurrentOutput: true
+      },
+      dev: [
+        'compass:dev',
+        'exec:serve',
+        'watch'
+      ]
+    },
     copy: {
       build: {
         files: [
@@ -21,8 +54,49 @@ module.exports = function(grunt) {
           }
         ]
       }
+    },
+    exec: {
+      build: {
+        cmd: 'jekyll build'
+      },
+      serve: {
+        cmd: 'jekyll serve --watch'
+      }
+    },
+    watch: {
+      css: {
+        files: [
+          '_site/assets/css/*.css'
+        ],
+        options: {
+          livereload: false
+        }
+      },
+      html: {
+        files: [
+          '_site/**/*.html'
+        ],
+        options: {
+          livereload: false
+        }
+      },
+      js: {
+        files: [
+          '_site/assets/js/*.js'
+        ],
+        options: {
+          livereload: false
+        }
+      }
     }
-  })
+  });
+  //
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Tasks:
   grunt.registerTask("build", ["copy:build"]);
+  grunt.registerTask("dev", ["concurrent:dev"]);
 }
