2253cd673a38930797b20d712ce5100040ea4e31
Christopher Torgalson <manager@bedlamhotel.com>
2014-09-29 14:40:35 +0200

    Add todo to modallink.js file

--- js/modallink.js
+++ js/modallink.js
@@ -3,6 +3,9 @@
  * in the page *if* there's javascript (not much sense doing this otherwise...)
  *
  * Does not attempt to rewrite links if this *is* the contact page.
+ *
+ * @todo Don't even include the contact form in the template--lazy-load it once
+ *       the rest of the page has downloaded.
  */
 var pathname = '/contact.html';
 if (window.location.pathname !== pathname) {
